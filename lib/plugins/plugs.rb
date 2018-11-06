b83a8dae4ac6312389e2ab6ed46ccacf9a37125b
Theo M <td512@td512.io>
2018-11-07 00:58:24 +1300

    Fixes Issue #4
    Now catches ctrl-c and passes it to the interpreter running

--- lib/plugins/plugs.rb
+++ lib/plugins/plugs.rb
@@ -1,13 +1,19 @@
 # WARNING: DO NOT MODIFY THIS FILE! I CANNOT BE MORE EXPLICIT!
 # WARNING: THIS FILE LOADS THE RIGHT PLUG FOR THE LANGUAGE. *DO NOT TOUCH ME*!
 def plug_exec(language, plugin_name)
-  IO.popen("#{language} #{File.expand_path("~/.ezycli/plugs")}/php/#{plugin_name}.php").each do |line|
+  io = IO.popen("#{language} #{File.expand_path("~/.ezycli/plugs")}/php/#{plugin_name}.php")
+  trap("INT") {
+    puts "#{info}Ctrl-C received, cleaning up"
+    Process.kill("INT", io.pid)
+  }
+  io.each do |line|
     if line.chomp.include? "Could not open input file"
       puts "#{error}Failed to run #{language} plug #{plugin_name}"
     else
       puts line.chomp
     end
   end
+  trap('INT', 'DEFAULT')
 end
 
 def plug_loader(language, plugin_name)
